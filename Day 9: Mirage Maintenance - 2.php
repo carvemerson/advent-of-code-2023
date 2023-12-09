45bda15e92f7a1dfec99969693a4a69916011a9b
Emerson Carvalho <carvemerson@gmail.com>
2023-12-09 08:49:59 -0300

    day 9 done

---
+++ Day 9: Mirage Maintenance - 2.php
@@ -0,0 +1,38 @@
+<?php
+
+$histories = [];
+while($input = fgets(STDIN)) {
+    $input = trim($input);
+    $input = explode(' ', $input);
+    $input = array_filter($input, fn($value) => $value !== '');
+    $histories[] = $input;
+}
+
+function getNextOfSequency($history) {
+    $last = $history[count($history)-1];
+    $valuesCount = array_count_values($history);
+
+    if (isset($valuesCount[0])) {
+        $zeros = $valuesCount[0];
+
+        if ($zeros === count($history)) {
+            return 0;
+        }
+    }
+
+    $newHistory = [];
+    for($i = 0; $i < count($history)-1; $i++) {
+        $newHistory[] = $history[$i+1] - $history[$i];
+    }
+
+    return $last + getNextOfSequency($newHistory);
+}
+
+
+$result = 0;
+foreach($histories as $history) {
+    $reverted = array_reverse($history);
+    $result += getNextOfSequency($reverted);
+}
+
+echo $result . PHP_EOL;
